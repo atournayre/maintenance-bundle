4152aca58645678fa349ce6d66a60b1ca3439597
atournayre <aurelien.tournayre@gmail.com>
2021-03-27 22:28:58 +0100

    Add tests and remove dead code.

---
+++ Tests/MaintenanceServiceTest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Atournayre\MaintenanceBundle\Tests;
+
+use Atournayre\Component\DotEnvEditor\DotEnvEditor;
+use Atournayre\MaintenanceBundle\Exception\MaintenanceInvalidIpException;
+use Atournayre\MaintenanceBundle\Exception\MaintenanceIpAlreadyDefinedException;
+use Atournayre\MaintenanceBundle\Service\MaintenanceService;
+use PHPUnit\Framework\TestCase;
+
+class MaintenanceServiceTest extends TestCase
+{
+    const PATH_TO_DOTENV_DOTTEST_DOTPHP = __DIR__.'/datas/.env.test.php';
+    /**
+     * @var MaintenanceService
+     */
+    private $maintenanceService;
+    /**
+     * @var DotEnvEditor
+     */
+    private $dotEnvEditor;
+
+    protected function setUp(): void
+    {
+        copy(__DIR__.'/datas/.env.php', self::PATH_TO_DOTENV_DOTTEST_DOTPHP);
+        $this->maintenanceService = new MaintenanceService();
+        $this->dotEnvEditor = new DotEnvEditor();
+    }
+
+    protected function tearDown(): void
+    {
+        unlink(self::PATH_TO_DOTENV_DOTTEST_DOTPHP);
+    }
+
+    public function testStartDateTime()
+    {
+        $startDateTime = new \DateTime();
+        $this->maintenanceService->start(self::PATH_TO_DOTENV_DOTTEST_DOTPHP, $startDateTime);
+        $this->dotEnvEditor->load(self::PATH_TO_DOTENV_DOTTEST_DOTPHP);
+        $this->assertEquals(
+            $startDateTime->format('Y-m-d H:i:s'),
+            (new \DateTime($this->dotEnvEditor->get('MAINTENANCE_START_DATETIME')))->format('Y-m-d H:i:s')
+        );
+    }
+
+    public function testEnable()
+    {
+        $this->maintenanceService->enable(self::PATH_TO_DOTENV_DOTTEST_DOTPHP);
+        $this->dotEnvEditor->load(self::PATH_TO_DOTENV_DOTTEST_DOTPHP);
+        $this->assertTrue($this->dotEnvEditor->get('MAINTENANCE_IS_ENABLED'));
+    }
+
+    public function testDisable()
+    {
+        $this->maintenanceService->disable(self::PATH_TO_DOTENV_DOTTEST_DOTPHP);
+        $this->dotEnvEditor->load(self::PATH_TO_DOTENV_DOTTEST_DOTPHP);
+        $this->assertFalse($this->dotEnvEditor->get('MAINTENANCE_IS_ENABLED'));
+    }
+
+    public function testAddIncorrectIpV4ThrowException()
+    {
+        $this->expectException(MaintenanceInvalidIpException::class);
+        $this->maintenanceService->addIp(self::PATH_TO_DOTENV_DOTTEST_DOTPHP, '192.168..');
+    }
+
+    public function testAddIncorrectIpV6ThrowException()
+    {
+        $this->expectException(MaintenanceInvalidIpException::class);
+        $this->maintenanceService->addIp(self::PATH_TO_DOTENV_DOTTEST_DOTPHP, '56FE::2159:5BBC::6594');
+    }
+
+    public function testAddIpV4()
+    {
+        $this->maintenanceService->addIp(self::PATH_TO_DOTENV_DOTTEST_DOTPHP, '127.0.0.1');
+        $this->dotEnvEditor->load(self::PATH_TO_DOTENV_DOTTEST_DOTPHP);
+        $this->assertEquals('127.0.0.1', $this->dotEnvEditor->get('MAINTENANCE_AUTHORIZED_IPS'));
+    }
+
+    public function testAddIpV4AlreadyAddedThrowException()
+    {
+        $this->expectException(MaintenanceIpAlreadyDefinedException::class);
+        $this->maintenanceService->addIp(self::PATH_TO_DOTENV_DOTTEST_DOTPHP, '127.0.0.1');
+        $this->maintenanceService->addIp(self::PATH_TO_DOTENV_DOTTEST_DOTPHP, '127.0.0.1');
+    }
+
+    public function testAddIpSpecialLocalhost()
+    {
+        $this->maintenanceService->addIp(self::PATH_TO_DOTENV_DOTTEST_DOTPHP, 'localhost');
+        $this->dotEnvEditor->load(self::PATH_TO_DOTENV_DOTTEST_DOTPHP);
+        $this->assertEquals('localhost', $this->dotEnvEditor->get('MAINTENANCE_AUTHORIZED_IPS'));
+    }
+
+    public function testCleanIpsGetPreviousIps()
+    {
+        $this->maintenanceService->addIp(self::PATH_TO_DOTENV_DOTTEST_DOTPHP, 'localhost');
+        $cleanedIps = $this->maintenanceService->cleanIps(self::PATH_TO_DOTENV_DOTTEST_DOTPHP);
+        $this->assertEquals(['localhost'], $cleanedIps);
+
+    }
+
+    public function testCleanIpsGetNewIps()
+    {
+        $this->maintenanceService->addIp(self::PATH_TO_DOTENV_DOTTEST_DOTPHP, 'localhost');
+        $this->maintenanceService->cleanIps(self::PATH_TO_DOTENV_DOTTEST_DOTPHP);
+        $this->maintenanceService->addIp(self::PATH_TO_DOTENV_DOTTEST_DOTPHP, '127.0.0.1');
+
+        $this->dotEnvEditor->load(self::PATH_TO_DOTENV_DOTTEST_DOTPHP);
+        $this->assertEquals('127.0.0.1', $this->dotEnvEditor->get('MAINTENANCE_AUTHORIZED_IPS'));
+
+    }
+}
+
---
+++ Tests/datas/.env.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'MAINTENANCE_IS_ENABLED' => false,
+    'MAINTENANCE_START_DATETIME' => '',
+    'MAINTENANCE_AUTHORIZED_IPS' => '',
+);
